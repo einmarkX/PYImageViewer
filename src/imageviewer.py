98a0a2db8e9be56fb2fdaaf14e88792c4f06ac49
floppvs <76272409+floppvs@users.noreply.github.com>
2021-07-02 20:06:46 +0700

    Update imageviewer.py

--- src/imageviewer.py
+++ src/imageviewer.py
@@ -1,5 +1,25 @@
 #/usr/bin/env python3
 
+#: PYImageViewer, For Viewing various image file
+#: Copyright Maulana B. (C) 2020-2021
+#:
+#: This Program is free software: you can redistribute it and/or modify
+#: it under terms of the GNU General Public License as published by
+#: Free Software Foundation , either version 3 of the License, or
+#: (at your option) any later version
+#:
+#:
+#: This program is distributed in the hope that it will be useful,
+#: but WITHOUT ANY WARRANTY; without even the implied warranty of
+#: MERCHANTIBILTY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+#: GNU General Public License for more details
+#:
+#: You should have received a copy of the GNU General Public License
+#: along with this program. If not, see <https://www.gnu.org/licenses/>
+#:
+#:
+#:          <Maintained Under>
+#:   Maulana B. <https://github.com/floppvs
 import gi
 gi.require_version('Gtk','3.0')
 
